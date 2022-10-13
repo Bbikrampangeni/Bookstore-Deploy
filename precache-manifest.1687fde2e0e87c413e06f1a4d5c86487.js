ccda358894074a72eef4ff99de6ca84e67dcd322
Bbikrampangeni <33446541+Bbikrampangeni@users.noreply.github.com>
2022-10-13 18:23:22 +0100

    Updates

---
+++ precache-manifest.1687fde2e0e87c413e06f1a4d5c86487.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "92036f80af14ed828886",
+    "url": "/Bookstore-Deploy/static/css/main.8f91c8cc.chunk.css"
+  },
+  {
+    "revision": "92036f80af14ed828886",
+    "url": "/Bookstore-Deploy/static/js/main.92036f80.chunk.js"
+  },
+  {
+    "revision": "0edb27e0bad5e19cc719",
+    "url": "/Bookstore-Deploy/static/js/1.0edb27e0.chunk.js"
+  },
+  {
+    "revision": "83dc453286f3019da560",
+    "url": "/Bookstore-Deploy/static/css/2.d11ec9f0.chunk.css"
+  },
+  {
+    "revision": "83dc453286f3019da560",
+    "url": "/Bookstore-Deploy/static/js/2.83dc4532.chunk.js"
+  },
+  {
+    "revision": "f2665920e940079a5085",
+    "url": "/Bookstore-Deploy/static/js/runtime~main.f2665920.js"
+  },
+  {
+    "revision": "fff5b9a766f89616e7438c2a1acbe0b2",
+    "url": "/Bookstore-Deploy/index.html"
+  }
+];
