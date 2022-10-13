c6b491a22ad0b462606f206f12c6e00830b26f44
Bbikrampangeni <33446541+Bbikrampangeni@users.noreply.github.com>
2022-10-13 18:21:31 +0100

    deploy website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { AgGridColumn, AgGridReact } from "ag-grid-react";
+import Appbar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import AddBook from "./AddBook";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import TodoTable from "./TodoTable";
+import "./App.css";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-material.css";
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const fetchBooks = () => {
+    fetch(
+      "https://bookstore-fa515-default-rtdb.europe-west1.firebasedatabase.app/booklist/books/.json"
+    )
+      .then((response) => response.json())
+      .then((data) => addKeys(data))
+      .catch((err) => console.error(err));
+  };
+
+  //Add keys to the todo objects
+  const addKeys = (data) => {
+    const keys = Object.keys(data);
+    const valueKeys = Object.values(data).map((book, index) =>
+      Object.defineProperty(book, "id", { value: keys[index] })
+    );
+    setBooks(valueKeys);
+  };
+
+  const addBook = (newBook) => {
+    fetch(
+      "https://bookstore-fa515-default-rtdb.europe-west1.firebasedatabase.app/booklist/books/.json",
+      {
+        method: "POST",
+        body: JSON.stringify(newBook),
+      }
+    )
+      .then((response) => fetchBooks())
+      .catch((err) => console.error(err));
+  };
+
+  const deleteBook = (id) => {
+    fetch(
+      `https://bookstore-fa515-default-rtdb.europe-west1.firebasedatabase.app/booklist/books/${id}.json`,
+      {
+        method: "DELETE",
+      }
+    )
+      .then((response) => fetchBooks())
+      .catch((err) => console.error(err));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Appbar position="static">
+        <Toolbar>
+          <Typography variant="h5" noWrap>
+            Bookstore
+          </Typography>
+        </Toolbar>
+      </Appbar>
+      <AddBook addBook={addBook} />
+      <div
+        className="ag-theme-material"
+        style={{ height: 400, width: 1100, margin: "auto" }}
+      >
+        <AgGridReact rowData={books}>
+          <AgGridColumn sortable={true} filter={true} field="title" />
+          <AgGridColumn sortable={true} filter={true} field="author" />
+          <AgGridColumn sortable={true} filter={true} field="year" />
+          <AgGridColumn sortable={true} filter={true} field="isbn" />
+          <AgGridColumn sortable={true} filter={true} field="price" />
+          <AgGridColumn
+            headerName=""
+            field="id"
+            width={90}
+            cellRendererFramework={ params => (
+              <IconButton
+                onClick={() => deleteBook(params.value)}
+                size="small"
+                color="secondary"
+              >
+                <DeleteIcon />
+              </IconButton>
+            )}
+          />
+        </AgGridReact>
+      </div>
     </div>
   );
 }
